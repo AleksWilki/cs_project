df64dc5c59fd465ed65496b3c38ca95884dba466
AleksanderWilkinson <35219878+AleksanderWilkinson@users.noreply.github.com>
2020-02-27 22:37:27 +0000

    Add connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,25 @@ const server = require('http').createServer(app);
 const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use('/', express.Router());
+const mongoose = require("mongoose");
+const dbName = "cs_project"
+app.use('/', express.Router())
 
 app.set("port", process.env.PORT || port);
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-server.listen(port, () => {
-    console.log(`AHM Server Launched At http://localhost:${port}`);
+mongoose.connect(
+    `mongodb://localhost:27017/${dbName}`,
+    { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+    console.log(`Successfully Connected To MongoDB: ${dbName}`);
+});
+
+server.listen(app.get("port"), () => {
+    console.log(`AHM Server Launched At http://localhost:${app.get("port")}`)
 });
 
 module.exports = app;
