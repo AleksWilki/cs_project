97a7ebabeec28961e54208dff8e1f2787bd9fec1
STEVEN TOMLINSON <b6023855@hallam.shu.ac.uk>
2020-03-15 15:37:57 +0000

    Baisc front end
    Basic front end developed for watch with simulation of swiping added too

--- frontend_watch/src/App.js
+++ frontend_watch/src/App.js
@@ -2,31 +2,21 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import {BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import Home from "./components/home";
 import HR from "./components/heartRate";
-import Sleep from "./components/sleep";
 import BP from "./components/bloodPreasure";
 import Steps from "./components/stepCounter";
-import Drink from "./components/drinkIntake";
-import Food from "./components/foodIntake";
-import Login from "./components/login";
-
-// <Redirect exact from="/" to="/login" /> to be added when login is working
+import Sleep from "./components/sleep";
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <div className="container">
-                    <Redirect exact from="/" to="/login" />
-                    <Route path="/home" component={Home} />
+                    <Redirect exact from="/" to="/heart-rate" />
                     <Route path="/heart-rate" component={HR} />
-                    <Route path="/sleep" component={Sleep} />
                     <Route path="/blood-preasure" component={BP} />
                     <Route path="/step-counter" component={Steps} />
-                    <Route path="/drink-intake" component={Drink} />
-                    <Route path="/food-intake" component={Food} />
-                    <Route path="/login" component={Login} />
+                    <Route path="/sleep" component={Sleep} />
                 </div>
             </Router>
         )
---
+++ frontend_watch/src/components/bloodPreasure.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'font-awesome/css/font-awesome.min.css';
+
+export class bloodPreasure extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div style={watchStyle}>
+                    <i className="fa fa-heartbeat fa-5x"></i>
+                    <br></br>
+                    <label style={{fontSize: "25px"}}>Blood Preasure is:</label>
+                    <h3 style={{fontSize: "50px"}}>140/90</h3>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <Link style={linkStyle} to="/heart-rate"><div style={{fontSize: "20px", float: "right"}}> <i className="fa fa-arrow-left fa-3x"></i> Scroll Left on Watch</div></Link>
+                    </div>
+                    <div className="col-md-4"></div>
+                    <div className="col-md-4">
+                        <Link style={linkStyle} to="/step-counter"><div style={{fontSize: "20px", float: "left"}}> Scroll Right on Watch<i className="fa fa-arrow-right fa-3x"></i></div></Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const watchStyle = {   // Css styling for the form
+    margin: "0 auto",
+    textAlign: 'center',
+    height: "450px",
+    width: "250px",
+    border: "10px solid black",
+    padding: "30px",
+    paddingTop: "30px"
+ 
+     }
+
+const linkStyle={
+    color: '#000',
+    textDecoration:'none'
+    }
+
+export default bloodPreasure
